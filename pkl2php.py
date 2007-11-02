22e67f0ef6cee2662e514e1a60bafa55eaa2a2c6
Stephen Soltesz <soltesz@cs.princeton.edu>
2007-11-02 18:07:11 +0000

    add additional options

--- pkl2php.py
+++ pkl2php.py
@@ -5,15 +5,20 @@ from config import config
 from optparse import OptionParser
 parser = OptionParser()
 parser.set_defaults(filename=None)
-parser.add_option("-f", "--dbname", dest="dbname", metavar="FILE", 
-					help="Provide the input file to convert")
+parser.add_option("-i", "--idb", dest="input", metavar="dbname", 
+					help="Provide the input dbname to convert")
+parser.add_option("-o", "--odb", dest="output", metavar="dbname", 
+					help="Provide the output dbname to save to")
 config = config(parser)
 config.parse_args()
 
-if config.dbname is None:
+if config.input is None:
 	print "please provide a pickle file to convert"
 	import sys
 	sys.exit(1)
+if config.output is None:
+	# just use the input name.
+	config.output = config.input
 
-data = soltesz.dbLoad(config.dbname)
-soltesz.dbDump(config.dbname, data, 'php')
+data = soltesz.dbLoad(config.input)
+soltesz.dbDump(config.output, data, 'php')
