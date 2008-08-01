be0ed37dd4d6ed52f487a2bb10b543eae0344767
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-08-01 17:36:10 +0000

    a unified configuration file for monitor scripts. all usernames and passwords should go here

---
+++ monitorconfig.py
@@ -0,0 +1,18 @@
+# RT DB
+RT_DB_HOST="rt.planet-lab.org"
+RT_DB_USER=""
+RT_DB_PASSWORD=""
+RT_DB_NAME=""
+
+# RT Web user account
+RT_WEB_TOOLS_PATH = "/"
+RT_WEB_SERVER   = "https://rt.planet-lab.org/"
+RT_WEB_USER     = ""
+RT_WEB_PASSWORD = ""
+RT_WEB_DEBUG    = "0"
+
+# PLC admin account
+API_SERVER = "https://boot.planet-lab.org/PLCAPI/"
+API_AUTH   = {'Username': '', 
+				'AuthMethod': 'password', 
+				'AuthString': ''}
