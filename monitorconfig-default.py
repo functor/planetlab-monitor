d1f853e1768bcffcae0aba5938359f18fbb96bd1
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-08-01 20:36:28 +0000

    so avoid getting this messed up with my local copy all the time.

---
+++ monitorconfig-default.py
@@ -0,0 +1,22 @@
+# RT DB
+RT_DB_HOST="rt.planet-lab.org"
+RT_DB_USER=""
+RT_DB_PASSWORD=""
+RT_DB_NAME=""
+
+# RT Web user account
+RT_WEB_TOOLS_PATH = "/"
+RT_WEB_SERVER   = "https://rt.planet-lab.org/"
+RT_WEB_USER     = ""
+RT_WEB_PASSWORD = ""
+RT_WEB_DEBUG    = "0"
+
+# PLC admin account
+API_SERVER = "https://boot.planet-lab.org/PLCAPI/"
+API_AUTH   = {'Username': '', 
+				'AuthMethod': 'password', 
+				'AuthString': ''}
+
+# SERVER PATHS
+MONITOR_SCRIPT_ROOT = "/home/soltesz/monitor"
+MONITOR_DATA_ROOT = "/home/soltesz/monitor/pdb"
