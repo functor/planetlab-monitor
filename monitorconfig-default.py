352fdcf0dc8991f1b0db2aabfc52e06ecf9ae99b
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-08-04 21:53:26 +0000

    no explicit auth structures in python to enable easier parsing by bash and php

--- monitorconfig-default.py
+++ monitorconfig-default.py
@@ -5,18 +5,18 @@ RT_DB_PASSWORD=""
 RT_DB_NAME=""
 
 # RT Web user account
-RT_WEB_TOOLS_PATH = "/"
-RT_WEB_SERVER   = "https://rt.planet-lab.org/"
-RT_WEB_USER     = ""
-RT_WEB_PASSWORD = ""
-RT_WEB_DEBUG    = "0"
+RT_WEB_TOOLS_PATH="/"
+RT_WEB_SERVER="https://rt.planet-lab.org/"
+RT_WEB_USER=""
+RT_WEB_PASSWORD=""
+RT_WEB_DEBUG="0"
 
 # PLC admin account
-API_SERVER = "https://boot.planet-lab.org/PLCAPI/"
-API_AUTH   = {'Username': '', 
-				'AuthMethod': 'password', 
-				'AuthString': ''}
+API_SERVER="https://boot.planet-lab.org/PLCAPI/"
+API_AUTH_USER=""
+API_AUTH_PASSWORD=""
 
 # SERVER PATHS
-MONITOR_SCRIPT_ROOT = "/home/soltesz/monitor"
-MONITOR_DATA_ROOT = "/home/soltesz/monitor/pdb"
+MONITOR_SCRIPT_ROOT="/home/soltesz/monitor"
+MONITOR_DATA_ROOT="/home/soltesz/monitor/pdb"
+MONITOR_ARCHIVE_ROOT="/home/soltesz/monitor/archive-pdb"
