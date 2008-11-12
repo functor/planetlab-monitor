cecd0af513d09eba28379de25d2c8b16979f3adc
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-11-12 20:33:31 +0000

    add setup files for the monitor module

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+#from setuptools import setup, find_packages
+
+packages=['monitor', 'monitor.database', 'monitor.pcu',
+	  	'monitor.sources', 'monitor.util', 'monitor.wrapper' ]
+
+#packages = find_packages(exclude=('Rpyc', 'www', 'ssh', 'pyssh',
+#'Rpyc.Demo', 'Rpyc.Servers', 'www.HyperText'))
+print packages
+setup(name='MonitorModule',
+      version='1.1',
+      description='Monitor Utility Module',
+      author='Stephen Soltesz',
+      author_email='soltesz@cs.princeton.edu',
+      url='http://www.planet-lab.org',
+	  packages=packages,
+)
+
