13701d18d65fac36dd9f9c788c63a3a4d0889bcc
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-11-25 22:40:39 +0000

    remove several files. add pcucontrol to setup.py updated Monitor.spec with pcucontrol

--- configparse.py
+++
@@ -1,5 +0,0 @@
-from xml.sax import saxutils
-
-class parse(saxutils.DefaultHandler):
-	def __init__(self, title, number):
-		self.search_title, self.search_number = title, number
--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python
 
 from distutils.core import setup
-#from setuptools import setup, find_packages
 
 packages=['monitor', 'monitor.database', 'monitor.database.zabbixapi', 
 		'monitor.database.info', 'monitor.pcu', 'monitor.sources', 
 		'monitor.util', 'monitor.wrapper' ]
 
-#packages = find_packages(exclude=('Rpyc', 'www', 'ssh', 'pyssh',
-#'Rpyc.Demo', 'Rpyc.Servers', 'www.HyperText'))
 print packages
 setup(name='MonitorModule',
       version='1.1',
       description='Monitor Utility Module',
+      author='Stephen Soltesz',
+      author_email='soltesz@cs.princeton.edu',
+      url='http://www.planet-lab.org',
+	  packages=packages,
+)
+
+packages=['pcucontrol', 
+		'pcucontrol.transports',
+		'pcucontrol.transports.ssh',
+		'pcucontrol.transports.pyssh',
+		'pcucontrol.models',
+		'pcucontrol.models.hpilo',
+		'pcucontrol.models.hpilo.iloxml',
+		'pcucontrol.models.intelamt',
+		'pcucontrol.models.intelamt',
+
+	]
+
+# TODO: add data dir for intelamt and hpilo stuff
+print packages
+setup(name='PCUControlModule',
+      version='1.1',
+      description='PCU Control Module',
       author='Stephen Soltesz',
       author_email='soltesz@cs.princeton.edu',
       url='http://www.planet-lab.org',
