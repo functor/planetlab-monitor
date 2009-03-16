842a7a17574e6fec92f219ceb35332a1bcafa3f7
Stephen Soltesz <soltesz@cs.princeton.edu>
2009-03-16 16:42:06 +0000

    more meaningful exception messages to help track down problems.

--- reboot.py
+++ reboot.py
@@ -332,7 +332,7 @@ class IPAL(PCUControl):
 		print "Current status is '%s'" % ret
 
 		if ret == '':
-			raise Exception("Status returned 'another session already open' %s : %s" % (node_port, ret))
+			raise Exception("Status returned 'another session already open' on %s %s : %s" % (self.host, node_port, ret))
 			
 				
 		if node_port < len(ret):
@@ -344,9 +344,9 @@ class IPAL(PCUControl):
 				# down
 				power_on = False
 			else:
-				raise Exception("Unknown status for PCU socket %s : %s" % (node_port, ret))
+				raise Exception("Unknown status for PCU %s socket %s : %s" % (self.host, node_port, ret))
 		else:
-			raise Exception("Mismatch between configured port and PCU status: %s %s" % (node_port, ret))
+			raise Exception("Mismatch between configured port and PCU %s status: %s %s" % (self.host, node_port, ret))
 			
 
 		if not dryrun:
@@ -372,9 +372,9 @@ class IPAL(PCUControl):
 					# down
 					power_on = False
 				else:
-					raise Exception("Unknown status for PCU socket %s : %s" % (node_port, ret))
+					raise Exception("Unknown status for PCU %s socket %s : %s" % (self.host, node_port, ret))
 			else:
-				raise Exception("Mismatch between configured port and PCU status: %s %s" % (node_port, ret))
+				raise Exception("Mismatch between configured port and PCU %s status: %s %s" % (self.host, node_port, ret))
 
 			if power_on:
 				return 0
