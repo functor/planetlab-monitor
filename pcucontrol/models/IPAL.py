6e0002914c3efb262f55cb250a0627b40659ccbf
Stephen Soltesz <soltesz@cs.princeton.edu>
2009-03-17 18:59:06 +0000

    on branch.

--- pcucontrol/models/IPAL.py
+++ pcucontrol/models/IPAL.py
@@ -100,6 +100,8 @@ class IPAL(PCUControl):
 			elif status == '0':
 				# down
 				power_on = False
+			elif status == '6':
+				raise ExceptionPort("IPAL reported 'Cable Error' on %s socket %s : %s" % (self.host, node_port, ret))
 			else:
 				raise Exception("Unknown status for PCU %s socket %s : %s" % (self.host, node_port, ret))
 		else:
@@ -128,6 +130,8 @@ class IPAL(PCUControl):
 				elif status == '0':
 					# down
 					power_on = False
+				elif status == '6':
+					raise ExceptionPort("IPAL reported 'Cable Error' on %s socket %s : %s" % (self.host, node_port, ret))
 				else:
 					raise Exception("Unknown status for PCU %s socket %s : %s" % (self.host, node_port, ret))
 			else:
