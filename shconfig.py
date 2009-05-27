936a0e571de292b777c3f71ab7dac11b9ec258c1
Barış Metin <Talip-Baris.Metin@sophia.inria.fr>
2009-05-27 09:09:27 +0000

    quote values

--- shconfig.py
+++ shconfig.py
@@ -5,4 +5,4 @@ from monitor import config
 for attr in dir(config):
 	val = config.__getattribute__(attr)
 	if attr[0].isupper() and attr[1].isupper():
-		print "%s=%s " % (attr, val)
+		print '%s="%s" ' % (attr, val)
