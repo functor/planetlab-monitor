d5ee3a6436fe36badf664affa42b03c566911906
Stephen Soltesz <soltesz@cs.princeton.edu>
2009-07-27 16:22:43 +0000

    make zabbix import conditional

--- monitor/database/dborm.py
+++ monitor/database/dborm.py
@@ -6,7 +6,8 @@ mon_metadata = sqlalchemy.MetaData()
 mon_metadata.bind = sqlalchemy.create_engine(config.monitor_dburi, echo=config.echo)
 mon_session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(autoflush=False,autocommit=True))
 
-#zab_metadata = sqlalchemy.MetaData()
-#zab_metadata.bind = sqlalchemy.create_engine(config.zabbix_dburi, echo=config.echo)
-#zab_session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(autoflush=False,autocommit=True))
-#zab_session.bind = zab_metadata.bind
+if config.zabbix_enabled:
+	zab_metadata = sqlalchemy.MetaData()
+	zab_metadata.bind = sqlalchemy.create_engine(config.zabbix_dburi, echo=config.echo)
+	zab_session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(autoflush=False,autocommit=True))
+	zab_session.bind = zab_metadata.bind
