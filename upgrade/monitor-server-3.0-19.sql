fa2476a41dcdf191df1026263fdaf1dd5ea3bedd
Stephen Soltesz <soltesz@cs.princeton.edu>
2009-07-27 19:58:58 +0000

    an upgrade script to extend convert existing databases

---
+++ upgrade/monitor-server-3.0-19.sql
@@ -0,0 +1,18 @@
+-- If there's an existing database, these commands will upgrade it to the
+-- current version
+
+ALTER TABLE findbadnoderecord ADD COLUMN firewall boolean DEFAULT False;
+ALTER TABLE findbadnoderecord_history ADD COLUMN firewall boolean DEFAULT False;
+
+ALTER TABLE findbadnoderecord ADD COLUMN external_dns_status boolean DEFAULT True;
+ALTER TABLE findbadnoderecord_history ADD COLUMN external_dns_status boolean DEFAULT True;
+
+ALTER TABLE findbadnoderecord ADD COLUMN rpms varchar DEFAULT NULL;
+ALTER TABLE findbadnoderecord_history ADD COLUMN rpms varchar DEFAULT NULL;
+
+ALTER TABLE findbadnoderecord ADD COLUMN uptime varchar DEFAULT NULL;
+ALTER TABLE findbadnoderecord_history ADD COLUMN uptime varchar DEFAULT NULL;
+
+ALTER TABLE findbadnoderecord ADD COLUMN traceroute varchar DEFAULT NULL;
+ALTER TABLE findbadnoderecord_history ADD COLUMN traceroute varchar DEFAULT NULL;
+
