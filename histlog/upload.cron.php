1778ddb80311e3d9a1d49d2ad9c6e2b095347219
Stephen Soltesz <soltesz@cs.princeton.edu>
2010-09-28 23:48:25 +0000

    run daily, with collection log script

--- histlog/upload.cron.php
+++ histlog/upload.cron.php
@@ -7,5 +7,5 @@ if (isset($_REQUEST['node_id'])) {
 $m = rand(0,59);
 $h = rand(0,23);
 echo "# random time on first day of week upload bash logs to monitor\n";
-echo "$m $h * * 0,3,5 root /usr/bin/collect_snoopylog.sh\n";
+echo "$m $h * * * root /usr/bin/collect_log.sh\n";
 ?>
